3ea1f43fecc405e1e54ba02ff37fbab19510ed68
Georg Wieland <georg.p.wieland@fau.de>
2017-10-29 01:58:50 +0200

    Added Coordinate class and CoordinateTest class

---
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -0,0 +1,214 @@
+/*
+ * Classname: Coordinate
+ * 
+ * Copyright (c) 2017 by Georg Wieland
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.wahlzeit.model;
+
+/**
+ * Coordinate is represented by a x, y and cartesian coordinate.
+ */
+public class Coordinate {
+	
+	//x coordinate
+	private double x = 0.0;
+	
+	//y coordinate
+	private double y = 0.0;
+	
+	//z coordinate
+	private double z = 0.0;
+	
+	/**
+	 * Constructs a Coordinate.
+	 */
+	public Coordinate() {
+
+	}
+	
+	/**
+	 * Constructs a Coordinate object with the given arguments.
+	 * 
+	 * @param x
+	 * x coordinate for initialization.
+	 * 
+	 * @param y
+	 * y coordinate for initialization.
+	 * 
+	 * @param z
+	 * z coordinate for initialization.
+	 */
+	public Coordinate(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	/**
+	 * Get method for x coordinate.
+	 * 
+	 * @return
+	 * Return x coordinate
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * Set method for x coordinate.
+	 * 
+	 * @param x
+	 * x coordinate
+	 */
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	/**
+	 * Get method for y coordinate.
+	 * 
+	 * @return
+	 * Return y coordinate
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * Set method for y coordinate.
+	 * 
+	 * @param y
+	 * y coordinate
+	 */
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	/**
+	 * Get method for z coordinate.
+	 * 
+	 * @return
+	 * Return z coordinate
+	 */
+	public double getZ() {
+		return z;
+	}
+
+	/**
+	 * Set method for z coordinate.
+	 * 
+	 * @param z
+	 * z coordinate
+	 */
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+
+	
+	/**
+	 * Calculates the cartesian distance.
+	 * 
+	 * @param otherCoordinate
+	 * Other Coordinate object for distance calculation.
+	 * 
+	 * @return
+	 * Calculated cartesian distance.
+	 */
+	public double getDistance(Coordinate otherCoordinate) {
+		
+		//check for Null value
+		if (otherCoordinate == null) {
+			return Double.NaN;
+		}
+		
+		//calculate difference for x
+		double tempDeltaX = otherCoordinate.getX() - this.x;
+		//calculate difference for y
+		double tempDeltaY = otherCoordinate.getY() - this.y;
+		//calculate difference for z
+		double tempDeltaZ = otherCoordinate.getZ() - this.z;
+		
+		//calculate and return cartesian distance
+		return Math.sqrt((tempDeltaX*tempDeltaX) + (tempDeltaY*tempDeltaY) + (tempDeltaZ*tempDeltaZ));
+	}
+	
+
+	/**
+	 * Checks if the two coordinate object are equal.
+	 * 
+	 * @param otherCoordinate
+	 * Other Coordinate object for comparison.
+	 * 
+	 * @return
+	 * True if equal otherwise false.
+	 */
+	public boolean isEqual(Coordinate otherCoordinate) {
+		if (otherCoordinate == null) {
+			return false;
+		}
+		
+		return ((this.x == otherCoordinate.getX()) && (this.y == otherCoordinate.getY()) && (this.z == otherCoordinate.getZ()));
+	}
+
+	/**
+	 * Overriding equals method.
+	 */
+	@Override
+	public boolean equals(Object obj) {	
+		
+		//check for comparison with itself
+		if (obj == this) {
+			return true;
+		}
+		
+		//check for Null value
+		if (obj == null) {
+			return false;
+		}
+		
+		//Check if obj is an instance of class Coordinate
+		if (!(obj instanceof Coordinate)) {
+			return false;
+		}
+		
+		//comparing data by using isEqual-method
+		return this.isEqual((Coordinate) obj);
+	}
+	
+	/**
+	 * It is recommended to override the hashcode method when overriding the equals method 
+	 * because equal objects may get different hash-values. This may lead to not properly 
+	 * working hash based collections.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+}//end of class Coordinate
+
---
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -0,0 +1,144 @@
+/*
+ * Classname: Coordinate
+ * 
+ * Copyright (c) 2017 by Georg Wieland
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.wahlzeit.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for {@link Coordinate} which contains all test cases.
+ *
+ */
+public class CoordinateTest {
+	Coordinate defaultCoordinate;
+	Coordinate parameterizedCoordinate;
+	
+	@Before
+	public void setupCoordinates() {
+		defaultCoordinate = new Coordinate();
+		parameterizedCoordinate = new Coordinate(1.0, 2.0, 3.0);
+	}
+	
+	//*************************************************************************
+	//							Coordinate.constructors
+	//*************************************************************************
+	@Test
+	public void testDefaultConstructor() {
+		assertEquals(0.0, defaultCoordinate.getX(), 0.0);
+		assertEquals(0.0, defaultCoordinate.getY(), 0.0);
+		assertEquals(0.0, defaultCoordinate.getZ(), 0.0);
+	}
+	
+	@Test
+	public void testParameterizedConstructor() {
+		assertEquals(1.0, parameterizedCoordinate.getX(), 0.0);
+		assertEquals(2.0, parameterizedCoordinate.getY(), 0.0);
+		assertEquals(3.0, parameterizedCoordinate.getZ(), 0.0);
+	}
+	
+	
+	//*************************************************************************
+	//							Coordinate.set-/get-
+	//*************************************************************************
+	@Test
+	public void testSetterAndGetter() {
+		defaultCoordinate.setX(3);
+		defaultCoordinate.setY(4);
+		defaultCoordinate.setZ(5);
+		assertEquals(3.0, defaultCoordinate.getX(), 0.0);
+		assertEquals(4.0, defaultCoordinate.getY(), 0.0);
+		assertEquals(5.0, defaultCoordinate.getZ(), 0.0);
+	}
+	
+	
+	//*************************************************************************
+	//							Coordinate.getDistance
+	//*************************************************************************
+	@Test
+	public void testGetDistanceNull() {
+		//check null value
+		assertEquals(Double.NaN, defaultCoordinate.getDistance(null), 0.0);
+	}
+	
+	@Test
+	public void testGetDistanceItself() {
+		assertEquals(0, defaultCoordinate.getDistance(defaultCoordinate), 0.0);
+		assertEquals(0, parameterizedCoordinate.getDistance(parameterizedCoordinate), 0.0);
+	}
+	
+	@Test
+	public void testGetDistanceCalculation() {		
+		assertEquals(3.741657, defaultCoordinate.getDistance(parameterizedCoordinate), 0.000001);	
+		assertEquals(3.741657, parameterizedCoordinate.getDistance(defaultCoordinate), 0.000001);
+	}
+	
+	
+	//*************************************************************************
+	//							Coordinate.isEqual
+	//*************************************************************************
+	@Test
+	public void testIsEqualNull() {
+		assertFalse(defaultCoordinate.isEqual(null));
+	}
+	
+	@Test
+	public void testIsEqualSameObject() {
+		assertTrue(defaultCoordinate.isEqual(defaultCoordinate));
+	}
+	
+	@Test
+	public void testIsEqualNotEqual() {
+		assertFalse(defaultCoordinate.isEqual(parameterizedCoordinate));
+	}
+	
+	
+	//*************************************************************************
+	//							Coordinate.equals
+	//*************************************************************************
+	@Test
+	public void testEqualNull() {
+		assertFalse(defaultCoordinate.equals(null));
+	}
+	
+	@Test
+	public void testEqualSameObject() {
+		assertTrue(defaultCoordinate.equals(defaultCoordinate));
+	}
+	
+	@Test
+	public void testEqualOtherObjectType() {
+		assertFalse(defaultCoordinate.equals(new Object()));
+	}
+	
+	@Test
+	public void testEqualNotEqual() {
+		assertFalse(defaultCoordinate.equals(parameterizedCoordinate));
+		assertFalse(parameterizedCoordinate.equals(new Coordinate(2.0, 2.0, 3.0)));
+		assertFalse(parameterizedCoordinate.equals(new Coordinate(1.0, 1.0, 3.0)));
+		assertFalse(parameterizedCoordinate.equals(new Coordinate(1.0, 2.0, 2.0)));
+	}
+	
+}
